d122751ccded11d33b4c2a48e38db41d85289e65
Julian Biebelberg <jbiebelberg@gmail.com>
2016-09-05 23:59:46 -0400

    email logic works and succesfully sends mail

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var nodemailer = require('nodemailer');
 var app = express();
 app.use(express.static('public'));
 var exphbs = require('express-handlebars');
@@ -29,6 +30,41 @@ app.get('/contact', function(req, res){
 
 app.get('/testomonial', function(req, res){
 	res.render('testomonial');
+})
+
+app.post('/mail', function(req, res){
+	//console.log(req.body);
+	var name = req.body.name;
+	var email = req.body.email;
+	var message = req.body.message;
+
+	var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'theicyreview@gmail.com', // Your email id
+            pass: 'kcdnsyuwwrzllpvl' // Your password
+        }
+    });
+
+    var mailOptions = {
+    from: 'theicyreview@gmail.com', // sender address
+    to: 'jbiebelberg@gmail.com', // list of receivers
+    subject: 'Email From Website', // Subject line
+    text: (message + "\r\n" + ' Contact email address is: ' + email + "\r\n" + 'Message sent from: ' + name) 
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        //console.log(error);
+        res.json({yo: 'error'});
+    }
+    else{
+        //console.log('Message sent: ' + info.response);
+        res.json({yo: info.response});
+    };
+});
+
+	//res.json(req.body);
 })
 
 var port = 3000;
