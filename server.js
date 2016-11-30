a003c545096f8bbb7ab14815b9aaeb0640baa59e
Julian Biebelberg <jbiebelberg@gmail.com>
2016-11-30 16:51:17 -0500

    revamped website

--- server.js
+++ server.js
@@ -2,11 +2,6 @@ var express = require('express');
 var nodemailer = require('nodemailer');
 var app = express();
 app.use(express.static('public'));
-var exphbs = require('express-handlebars');
-
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
-}));
 
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );
@@ -14,22 +9,8 @@ app.use(bodyParser.urlencoded({
   extended: true
 })); 
 
-app.set('view engine', 'handlebars');
-
 app.get('/', function(req, res){
 	res.render('index');
-})
-
-app.get('/portfolio', function(req, res){
-	res.render('portfolio');
-})
-
-app.get('/contact', function(req, res){
-	res.render('contact');
-})
-
-app.get('/testomonial', function(req, res){
-	res.render('testomonial');
 })
 
 app.post('/mail', function(req, res){
